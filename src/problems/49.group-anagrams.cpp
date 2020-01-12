7627695eef406b20a8bfe012472f37e0b79259b0
yingmin.li <yingmin.li@horizon.ai>
2020-01-12 19:31:32 +0800

    49

---
+++ src/problems/49.group-anagrams.cpp
@@ -0,0 +1,28 @@
+//
+// Created by liyingmin on 2020/1/12.
+//
+/*!
+ * @brief https://leetcode-cn.com/problems/group-anagrams
+ * Time: O(nmlogm), n = strs.size(), m = sum(strs[i].size())/n
+ * Space: O(n*m)+
+ */
+#include <string>
+#include <unordered_map>
+#include <vector>
+vector<vector<string>> groupAnagrams(vector<string> &strs) {
+  std::unordered_map<std::string, std::vector<int>> counter;
+  auto copy_strs = strs;
+  for (size_t i = 0; i < copy_strs.size(); ++i) {
+    std::sort(copy_strs[i].begin(), copy_strs[i].end());
+    counter[copy_strs[i]].push_back(i);
+  }
+  std::vector<std::vector<std::string>> ret;
+  for (auto &str_counter : counter) {
+    std::vector<std::string> anagrams;
+    for (auto &ele : str_counter.second) {
+      anagrams.push_back(strs[ele]);
+    }
+    ret.push_back(anagrams);
+  }
+  return ret;
+}
