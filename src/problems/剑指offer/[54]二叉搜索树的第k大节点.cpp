2faa1ef6f3d212581b453b0c6ca87138aa070fc7
liyingmin <liyingmin@kuaishou.com>
2021-05-26 16:50:16 +0800

    lcof 54

---
+++ src/problems/剑指offer/[54]二叉搜索树的第k大节点.cpp
@@ -0,0 +1,61 @@
+// 输入: root = [3,1,4,null,2], k = 1
+//   3
+//  / \
+// 1   4
+//  \
+//   2
+//输出: 4
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// 中序遍历递增
+class Solution {
+ public:
+  int kthLargest(TreeNode* root, int k) {
+    count_ = 0;
+    k_ = k;
+    recursion(root);
+    return val_;
+  }
+
+ private:
+  int count_;
+  int k_;
+  int val_;
+  void recursion(TreeNode* root) {
+    if (root == nullptr) return;
+    recursion(root->right);
+    count_++;
+    if (count_ == k_) {
+      val_ = root->val;
+      return;
+    }
+    recursion(root->left);
+  }
+};
+
+class Solution1 {
+ public:
+  int kthLargest(TreeNode* root, int k) {
+    recursion(root);
+    return val_[val_.size() - k];
+  }
+
+ private:
+  vector<int> val_;
+  void recursion(TreeNode* root) {
+    if (root == nullptr) return;
+    recursion(root->left);
+
+    val_.push_back(root->val);
+
+    recursion(root->right);
+  }
+};
