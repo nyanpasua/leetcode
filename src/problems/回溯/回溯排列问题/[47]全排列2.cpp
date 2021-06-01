b14e2187fa744a4ee7c14f336a8888bb729e91d5
liyingmin <liyingmin@kuaishou.com>
2021-06-01 18:22:37 +0800

    lc 47

--- src/problems/回溯/回溯排列问题/[47]全排列2.cpp
+++ src/problems/回溯/回溯排列问题/[47]全排列2.cpp
@@ -1,13 +1,15 @@
-// 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序
-// 返回答案。
-// 输入：nums = [1,2,3]
-//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// 输入：nums = [1,1,2]
+//输出：
+//[[1,1,2],
+// [1,2,1],
+// [2,1,1]]
+#include <algorithm>
 #include <unordered_set>
 #include <vector>
 using std::vector;
 class Solution {
  public:
-  vector<vector<int>> permute(vector<int>& nums) {
+  vector<vector<int>> permuteUnique(vector<int>& nums) {
     dfs(nums, 0);
     return res_;
   }
@@ -20,7 +22,13 @@ class Solution {
       res_.push_back(nums);
       return;
     }
+    std::unordered_set<int> visited;  // 这里其实无需
     for (int j = index; j < nums.size(); ++j) {
+      if (visited.find(nums[j]) != visited.end()) {
+        // 已经访问过，跳过该数字
+        continue;
+      }
+      visited.insert(nums[j]);
       // 固定 nums[j] 到 index 位置
       std::swap(nums[j], nums[index]);
       // 找下一个固定位
