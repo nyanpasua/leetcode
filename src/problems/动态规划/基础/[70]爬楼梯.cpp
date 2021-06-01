ba5cb40a2ca035308c18088d828a682640ca7cb2
liyingmin <liyingmin@kuaishou.com>
2021-06-01 17:46:48 +0800

    lc 70

--- src/problems/动态规划/基础/[70]爬楼梯.cpp
+++ src/problems/动态规划/基础/[70]爬楼梯.cpp
@@ -1,8 +1,27 @@
 //
 // Created by liyingmin on 2020/3/27.
-//
-// 暴力递归
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+#include <vector>
+using std::vector;
+// 动态规划
 class Solution {
+ public:
+  int climbStairs(int n) {
+    if (n == 1) {return 1;}
+    std::vector<int> memo(n + 1, -1);
+    memo[1] = 1;
+    memo[2] = 2;
+    for (int i = 3; i <= n; ++i) {
+      memo[i] = memo[i - 1] + memo[i - 2];
+    }
+    return memo[n];
+  }
+};
+
+// 暴力递归
+class Solution2 {
  public:
   int climbStairs(int n) {
     if (n == 1) {
@@ -16,7 +35,7 @@ class Solution {
 };
 
 // 记忆化递归
-class Solution {
+class Solution1 {
  private:
   std::vector<int> memo;
   int searchWays(int n) {
@@ -36,22 +55,5 @@ class Solution {
   int climbStairs(int n) {
     memo = std::move(vector<int>(n + 1, -1));
     return searchWays(n);
-  }
-};
-
-// 暴力回溯（略），模拟走的所有路径，计数求和
-
-// 动态规划
-class Solution {
- public:
-  int climbStairs(int n) {
-    if (n == 1) {return 1;}
-    std::vector<int> memo(n + 1, -1);
-    memo[1] = 1;
-    memo[2] = 2;
-    for (int i = 3; i <= n; ++i) {
-      memo[i] = memo[i - 1] + memo[i - 2];
-    }
-    return memo[n];
   }
 };
