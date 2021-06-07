b8117670911e0e9de4adc97f0f9437744f613ac8
liyingmin <liyingmin@kuaishou.com>
2021-06-07 11:25:18 +0800

    update

--- src/problems/动态规划/LIS问题/[300]最长递增子序列.cpp
+++ src/problems/动态规划/LIS问题/[300]最长递增子序列.cpp
@@ -1,6 +1,53 @@
+//给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。 
 //
-// Created by liyingmin on 2020/3/29.
+// 子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序
+//列。 
+// 
 //
+// 示例 1： 
+//
+// 
+//输入：nums = [10,9,2,5,3,7,101,18]
+//输出：4
+//解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,0,3,2,3]
+//输出：4
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [7,7,7,7,7,7,7]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 2500 
+// -104 <= nums[i] <= 104 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你可以设计时间复杂度为 O(n2) 的解决方案吗？ 
+// 你能将算法的时间复杂度降低到 O(n log(n)) 吗? 
+// 
+// Related Topics 二分查找 动态规划 
+// 👍 1646 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
 #include <vector>
 using std::vector;
 /// 二分查找+贪心
@@ -19,7 +66,7 @@ class Solution {
       } else {
         int l = 1, r = len,
             pos = 0;  // 如果找不到说明所有的数都比 nums[i] 大，此时要更新
-                      // d[1]，所以这里将 pos 设为 0
+        // d[1]，所以这里将 pos 设为 0
         while (l <= r) {
           int mid = (l + r) >> 1;
           if (d[mid] < nums[i]) {
@@ -57,3 +104,4 @@ class Solution1 {
     return *(std::max_element(memo.begin(), memo.end()));
   }
 };
+//leetcode submit region end(Prohibit modification and deletion)
