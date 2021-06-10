29cbd5226a09fe7ebcf5e24a84ee0a3884df65cd
liyingmin <liyingmin@kuaishou.com>
2021-06-10 15:04:22 +0800

    lc 148

--- src/problems/链表/复杂穿针引线和排序/[148]排序链表.cpp
+++ src/problems/链表/复杂穿针引线和排序/[148]排序链表.cpp
@@ -1,17 +1,58 @@
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。 
 //
-// Created by liyingmin on 2020/4/13.
+// 进阶： 
 //
+// 
+// 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？ 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 5 * 104] 内 
+// -105 <= Node.val <= 105 
+// 
+// Related Topics 排序 链表 
+// 👍 1169 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- * 思路：使用自顶向下的归并排序的思路，但是不像数组需要额外内存，链表重连即可
- * Time: O(nlogn)
- * Space: O(1) + 堆栈内存（栈深度logn）
  */
+
 class Solution {
  private:
   // Time: O(n)
@@ -64,3 +105,4 @@ class Solution {
 
   }
 };
+//leetcode submit region end(Prohibit modification and deletion)
